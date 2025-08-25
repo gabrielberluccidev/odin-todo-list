e668ef4a5f2e669a51fca766dbd8d0165d79118f
Gabriel Berlucci <gabrielberluccidev@proton.me>
2025-08-25 17:17:48 -0300

    feat: add the controller to the todo-list i.e click on the button and adding to the main container was moved to this archive

---
+++ src/tasks/taskController.js
@@ -0,0 +1,25 @@
+import { Task } from "./taskModel.js";
+import { getTaskFormInput, clearForm } from "./taskForm.js";
+import { createTodoCard } from "./taskUI.js";
+
+export function initTaskController() {
+  const btn = document.querySelector("#add-task-button");
+  const container = document.querySelector(".todo-list-main-task");
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const formData = getTaskFormInput();
+    if (!formData) return;
+
+    const task = new Task(
+      formData.todoTitleInput,
+      formData.todoDateInput,
+      formData.todoPriorityInput
+    );
+    const card = createTodoCard(task);
+
+    container.appendChild(card);
+    clearForm();
+  });
+}
