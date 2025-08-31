4d0dbda2b254431478f0e99230d0b6fd49dfb7d3
Gabriel Berlucci <gabrielberluccidev@proton.me>
2025-08-31 02:18:15 -0300

    feat: add a new event listener to order selection

--- src/tasks/taskController.js
+++ src/tasks/taskController.js
@@ -2,10 +2,12 @@ import { Task } from "./taskModel.js";
 import { getTaskFormInput, clearForm } from "./taskForm.js";
 import { createTodoCard, closeModal, openEditModal } from "./taskUI.js";
 import { checkExpiredDate } from "./checkExpiredDate.js";
+import { checkPriorityOrder } from "./checkPriorityOrder.js";
 
 export function initTaskController() {
   const btn = document.querySelector("#add-task-button");
   const container = document.querySelector(".todo-list-main-task");
+  const order = document.querySelector("#order");
 
   btn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -36,5 +38,9 @@ export function initTaskController() {
     if (e.target.classList.contains("delete")) {
       e.target.closest(".todo-list-main-task-card").remove();
     }
+  });
+
+  order.addEventListener("change", () => {
+    checkPriorityOrder();
   });
 }
