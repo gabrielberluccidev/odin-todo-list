ff9756eff30bc508b57b97f8d683e53ef9adbe6d
Gabriel Berlucci <gabrielberluccidev@proton.me>
2025-09-07 17:43:25 -0300

    feat: now it call two new functions (saveTasks, getTasks)

--- src/tasks/taskController.js
+++ src/tasks/taskController.js
@@ -3,11 +3,14 @@ import { getTaskFormInput, clearForm } from "./taskForm.js";
 import { createTodoCard, closeModal, openEditModal } from "./taskUI.js";
 import { checkExpiredDate } from "./checkExpiredDate.js";
 import { checkPriorityOrder } from "./checkPriorityOrder.js";
+import { getTasks, saveTask } from "./taskStorage.js";
 
 export function initTaskController() {
   const btn = document.querySelector("#add-task-button");
   const container = document.querySelector(".todo-list-main-task");
   const order = document.querySelector("#order");
+
+  getTasks();
 
   btn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -25,9 +28,10 @@ export function initTaskController() {
     container.appendChild(card);
 
     clearForm();
-    closeModal();
 
     setInterval(checkExpiredDate, 1000);
+    saveTask();
+    closeModal();
   });
 
   container.addEventListener("click", (e) => {
